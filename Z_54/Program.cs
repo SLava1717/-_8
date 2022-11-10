8107f4664ddb10d80127ff93e2de567208ca0e5d
Slava K <slava.kostromin.2000@mail.ru>
2022-11-10 12:57:07 +0700

    Z_54

---
+++ Z_54/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+
+*/
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = 0;
+                    temp = matrix[i, k];
+                    matrix[i, k] = matrix[i, k + 1];
+                    matrix[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк и столбцов через пробел: ");
+string[] num = Console.ReadLine().Split(' ');
+int n = int.Parse(num[0]);
+int m = int.Parse(num[1]);
+int[,] matrix = new int[n, m];
+Console.WriteLine("Begin: ");
+InputMatrix(matrix);
+Console.WriteLine("Result: ");
+ReleaseMatrix(matrix);
